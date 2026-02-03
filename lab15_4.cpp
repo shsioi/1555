600b93ad326677e270ef91b5cd61d133c65fa5f8
shsioi <dragonbass49@gmail.com>
2026-02-04 01:40:31 +0900

    lab15_comp_4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -5,17 +5,19 @@ using namespace std;
 
 void shuffle(int &,int &,int &,int &);
 
-int main(){
-	int a = 50, b = 100, c = 500, d = 1000;
-	
-	srand(time(0));	
-	
-	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
-	    cout << a << " " << b << " " << c << " " << d << endl;
-	}
-	
-	return 0;
+void shuffle(int &a, int &b, int &c, int &d){
+    int temp[] = {a,b,c,d};
+    
+    for(int i = 3; i > 0; i--){
+        int j = rand()%(i+1); 
+        int t = temp[i];
+        temp[i] = temp[j];
+        temp[j] = t;
+    }
+    a = temp[0];
+    b = temp[1];
+    c = temp[2];
+    d = temp[3];
 }
 
 //Do not modify source code above this line
